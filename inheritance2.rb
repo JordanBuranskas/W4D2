6b30428ecb029afa09702d4832a5acd6d83cc7cb
suzu <useNavigate@gmail.com>
2023-01-31 17:38:42 -0500

    99% done !

--- inheritance2.rb
+++ inheritance2.rb
@@ -1,19 +1,50 @@
 class Employee
-    def initialize(name,title,salary,boss)
-    @name = name
-    @title = title 
-    @salary = salary 
-    @boss = boss 
+
+    attr_reader :boss ,:salary
+    def initialize(name,title,salary,boss=nil)
+        @name = name
+        @title = title 
+        @salary = salary.to_i 
+        @boss = boss 
+        if @boss !=nil
+            self.boss.minions << self
+        end
     end
 
     def bonus(multi)
         bonus = @salary.to_i * multi
     end
+
 end
 
 class Manager < Employee 
-        def initialize(name,title,salary,boss)
+    attr_reader :minions 
+        def initialize(name,title,salary,boss=nil)
             super#(name,title,salary,boss)
-            @minions=[]
+           @minions=[]
         end
+
+        def bonus_manager(multi)
+          self.sub_salary  * multi
+        end
+
+        def sub_salary
+            minion_total=0
+           @minions.each do |minion|
+            if minion.is_a?(Manager)
+            minion_total+= minion.salary + minion.sub_salary 
+            else
+            minion_total+= minion.salary
+           end
+           minion_total
+        end
+    end
+
+    
 end
+
+# ceo =Manager.new("ceo","CEO",100)
+# manager = Manager.new("man","manager",50,ceo)
+# min2 = Employee.new("min2","slave","12",manager)
+# min1 = Employee.new("min1","slave","10",manager)
+
