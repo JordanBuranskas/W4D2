29e4149a8c6af1222ea21d9d5ddd5ebe6ee941f6
suzu <useNavigate@gmail.com>
2023-01-31 15:47:09 -0500

    bonus2

--- inheritance2.rb
+++ inheritance2.rb
@@ -1,1 +1,19 @@
+class Employee
+    def initialize(name,title,salary,boss)
+    @name = name
+    @title = title 
+    @salary = salary 
+    @boss = boss 
+    end
 
+    def bonus(multi)
+        bonus = @salary.to_i * multi
+    end
+end
+
+class Manager < Employee 
+        def initialize(name,title,salary,boss)
+            super#(name,title,salary,boss)
+            @minions=[]
+        end
+end
